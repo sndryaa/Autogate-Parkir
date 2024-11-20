726de9eb0aedf6de1979e52e639bc0e001905a6f
sndryaa <sandirayagumilang2@gmail.com>
2024-11-20 15:04:11 +0700

    WI1102_ProjekI_05_03 (Source Code).py

--- WI1102_ProjekI_05_03 (Source Code).py
+++ WI1102_ProjekI_05_03 (Source Code).py
@@ -1,4 +1,4 @@
-# PROJEK BERPIKIR KOMPUTASIONAL 1
+# PROYEK BERPIKIR KOMPUTASIONAL 1
 # AUTOGATE PARKIR
 
 # Kelompok 3 :
@@ -15,7 +15,7 @@ import time  # Untuk fungsi hitung mundur
 # Fungsi untuk hitung mundur dan menutup palang
 def hitung_mundur(detik):
     while detik > 0:
-        print(f"Palang akan menutup dalam {detik} detik...", end="")
+        print(f"Palang akan menutup dalam {detik} detik...")
         time.sleep(1)
         detik -= 1
     print()
@@ -33,7 +33,7 @@ def tutup_palang():
     else:
         print("Kendaraan tidak terdeteksi. Menghitung mundur untuk menutup palang.")
         hitung_mundur(5)  # Hitung mundur sama untuk semua jenis kendaraan
-    print("Palang parkir telah menutup.")
+    print("Palang parkir telah tertutup.")
 
 b = 0
 banyak = []  # Daftar untuk menyimpan data kendaraan yang masuk
@@ -72,16 +72,19 @@ while True:
                 print("Apakah data sudah valid?")
                 print("[1] Ya, [2] Tidak")
                 validasi = int(input())
+                print()
 
                 if validasi == 1:
                     # Cetak karcis sebelum deteksi portal
-                    print("[================================================]")
-                    print("[        KARCIS PARKIR ITB JATINANGOR            ]")
-                    print(f"[    Kendaraan : {kendaraan}                           ]")
-                    print(f"[    Nomor     : {nopol}                         ]")
-                    print(f"[    Waktu     : {waktu_sekarang}      ]")
-                    print("[     Kunci kendaraan anda dengan rapat          ]")
-                    print("[================================================]")
+                    print("[]================================================[]")
+                    print("         KARCIS PARKIR ITB JATINANGOR            ")
+                    print()
+                    print(f"     Kendaraan : {kendaraan}                           ")
+                    print(f"     Nomor     : {nopol}                        ")
+                    print(f"     Waktu     : {waktu_sekarang}      ")
+                    print()
+                    print("         Kunci kendaraan anda dengan rapat          ")
+                    print("[]================================================[]")
 
                     print()
                     # Deteksi kendaraan melewati portal menggunakan tutup_palang()
@@ -104,46 +107,130 @@ while True:
                     break
                 else:
                     print("Data tidak valid. Silakan coba lagi.")
+                    print()
                     break
             else:
                 print("Jenis kendaraan tidak valid")
                 print()
+
     elif menu == 2:
         print("- MENU KELUAR -")
         nopol = input("Masukkan nomor plat kendaraan: ")
 
-        i = 0
-        while i < b:
-            if b == 0:
-                print("Tidak ada kendaraan terparkir!")
-                print()
-                break
-            else:
-                karcis = banyak[i]
-                if karcis[1] != nopol:
-                    if i < b - 1:
-                        i += 1
-                    else:
-                        print("Kendaraan tidak ditemukan!")
-                        print()
+        if b == 0:
+            print("Tidak ada kendaraan terparkir!")
+            print()
+
+        else:
+            i = 0
+            while i < b:
+                    karcis = banyak[i]
+                    if karcis[1] != nopol:
+                        if i < b - 1:
+                            i += 1
+                        else:
+                            print("Kendaraan tidak ditemukan!")
+                            print()
+                            break
+
+                    elif karcis[1] == nopol:
+                        print(f"Kendaraan ditemukan: {karcis[0]} dengan plat nomor {karcis[1]}")
+                        # Input data waktu otomatis dari sistem
+                        waktu_sekarang = datetime.now()
+                        tanggal_keluar = int(waktu_sekarang.day)
+                        jam_keluar = int(waktu_sekarang.hour)
+                        menit_keluar = int(waktu_sekarang.minute)
+                        detik_keluar = int(waktu_sekarang.second)
+
+                        # Hitung durasi parkir
+                        total_detik_masuk = jam_masuk * 3600 + menit_masuk * 60 + detik_masuk
+                        total_detik_keluar = jam_keluar * 3600 + menit_keluar * 60 + detik_keluar
+                        durasi_hari = tanggal_keluar - karcis[2]
+                        durasi_detik = total_detik_keluar - total_detik_masuk
+
+                        if durasi_detik < 0:
+                            durasi_detik += 24 * 3600 # Tambahkan satu hari dalam detik jika melewati tengah malam
+
+                        total_durasi_menit = durasi_hari * 1440 + (durasi_detik // 60) # Total durasi dalam menit
+
+                        # Pembayaran
+                        biaya = 0
+                        if karcis[0].lower() == "motor":
+                            # Durasi parkir di hari pertama
+                            if durasi_hari == 0 :
+                                if total_durasi_menit <= 60 :
+                                    biaya = 1000  # 1 jam pertama
+
+                                else: # Biaya untuk jam berikutnya
+                                    biaya = 1000 + (total_durasi_menit - 60) // 60 * 1000  # setengah jam atau lebih
+                            else: # Durasi Hari lebih dari 1 hari
+                                biaya = 10000 + durasi_hari * 10000
+
+                        elif karcis[0].lower() == "mobil":
+                            # Durasi parkir di hari pertama
+                            if durasi_hari == 0 :
+                                if total_durasi_menit <= 60:
+                                    biaya = 5000  # 1 jam pertama
+                                else: # Biaya untuk jam berikutnya
+                                    biaya = 5000 + (total_durasi_menit - 60) // 60 * 5000
+                            else: # Durasi parkir lebih dari 1 hari
+                                biaya = 25000 +  durasi_hari * 25000
+
+                        # Tampilkan rincian
+                        durasi_jam = total_durasi_menit // 60
+                        sisa_menit = total_durasi_menit % 60
+
+                        print("Biaya parkir untuk kendaraan", nopol, "adalah Rp ",biaya)
+                        print("Durasi parkir:", durasi_hari, "hari", durasi_jam, "jam", sisa_menit, "menit")
+
+                        # Pembayaran
+                        while True:
+                            print("Pilih metode pembayaran: ")
+                            print("[1] Cash")
+                            print("[2] Kartu elektronik")
+                            metode_pembayaran = int(input("Masukkan pilihan: "))
+
+                            if metode_pembayaran == 1 :
+                                print("Silakan membayar sebesar Rp", biaya, "secara tunai.")
+                                time.sleep(3)
+                                print("Terima kasih, hati-hati di jalan!")
+                                break
+
+                            elif metode_pembayaran == 2 :
+                                print("Silakan tempel kartu anda!")
+                                saldo_kartu = int(input("Saldo kartu anda: "))
+
+                                if saldo_kartu >= biaya:
+                                    saldo_kartu -= biaya
+                                    print("Kartu anda tersisa Rp", saldo_kartu)
+                                    print("Terima kasih, hati-hati di jalan!")
+                                    break
+                                else:
+                                    print("Saldo kartu anda tidak mencukupi!")
+                                    continue # Kembali ke menu pembayaran
+
+                        # Tambahkan hitung mundur menutup palang portal
+                        if karcis[0].lower() == "motor":
+                            print()
+                            hitung_mundur(7)
+                        else:
+                            print()
+                            hitung_mundur(10)
+                        # Memastikan apakah kendaraan melewati palang
+                        tutup_palang()
+                        deteksi = cek_deteksi_kendaraan()
+                        if deteksi == 1:
+                            # Hapus kendaraan dari daftar
+                            del banyak[i]
+                            b -= 1
+                        else:
+                            print()
+
+                        print("==========================================")
+                        print("JUMLAH KENDARAAN TERPARKIR SAAT INI: ", b)
+                        print("==========================================")
                         break
-                elif karcis[1] == nopol:
-                    print(f"Kendaraan ditemukan: {karcis[0]} dengan nomorplat {karcis[1]}")
 
-                    # Memastikan apakah kendaraan melewati palang?
-                    tutup_palang()
-                    deteksi = cek_deteksi_kendaraan()
-                    if deteksi == 1:
-                        # Hapus kendaraan dari daftar
-                        del banyak[i]
-                        b -= 1
-                    else:
-                        print()
-
-                    print("==========================================")
-                    print("JUMLAH KENDARAAN TERPARKIR SAAT INI: ", b)
-                    print("==========================================")
-                    break
     elif menu == 3:
         print("Menutup program portal...")
         break
